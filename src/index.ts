1c4956d4c990c6754cf1b5c808d156a8bad8d0b5
Francine Hahn <fran_hahn@hotmail.com>
2023-02-08 19:30:33 -0300

    route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { app } from "./app"
+import { productRouter } from "./route/productRouter"
+import { purchaseRouter } from "./route/purchaseRouter"
+import { userRouter } from "./route/userRouter"
 
 
-
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+app.use("/purchases", purchaseRouter)
 
---
+++ src/route/purchaseRouter.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import { PurchaseBusiness } from "../business/PurchaseBusiness"
+import { PurchaseController } from "../controller/PurchaseController"
+import ProductDatabase from "../data/ProductDatabase"
+import PurchaseDatabase from "../data/PurchaseDatabase"
+
+
+export const purchaseRouter = express.Router()
+const purchaseDatabase = new PurchaseDatabase()
+const productDatabase = new ProductDatabase()
+const purchaseBusiness = new PurchaseBusiness(purchaseDatabase, productDatabase)
+const purchaseController = new PurchaseController(purchaseBusiness)
+
+purchaseRouter.get("/sales", purchaseController.getSalesByUserId)
+purchaseRouter.get("/", purchaseController.getPurchasesByUserId)
+purchaseRouter.post("/", purchaseController.createPurchase)
