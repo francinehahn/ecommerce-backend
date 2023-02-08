356df98e5230f624bd39dd558cef7c580ccc5a0f
Francine Hahn <fran_hahn@hotmail.com>
2023-02-08 19:19:23 -0300

    exclui a pasta routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { app } from "./app"
-import { ProductRouter } from "./routes/productRouter"
-import { PurchaseRouter } from "./routes/purchaseRouter"
-import { UserRouter } from "./routes/userRouter"
-
-
-app.use("/products", ProductRouter)
-app.use("/users", UserRouter)
-app.use("/purchases", PurchaseRouter)
 
 
 
