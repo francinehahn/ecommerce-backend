173dadaf9b4fc381f4321fde7987ffd02caace3f
Francine Hahn <fran_hahn@hotmail.com>
2023-03-07 18:25:51 -0300

    I crated the userDatabaseMock

--- tests/mocks/UserDatabaseMock.ts
+++ tests/mocks/UserDatabaseMock.ts
@@ -1,18 +1,16 @@
 import { UserRepository } from "../../src/business/UserRepository"
 import User, { updateUserInfoDTO } from "../../src/models/User"
+import { UsersMock } from "./UsersMock"
 
 
 export default class UserDatabaseMock implements UserRepository {
 
-    signup = async (newUser: User): Promise<void> => {
-        return
-    }
+    signup = async (newUser: User): Promise<void> => {}
 
-    editUserInfo = async (userInfo: updateUserInfoDTO): Promise<void> => {
-        
-    }
+    editUserInfo = async (userInfo: updateUserInfoDTO): Promise<void> => {}
 
     getUserBy = async (column: string, value: string): Promise<any> => {
-        
+        const filter = UsersMock.filter(item => item.email === value || item.id === value)
+        return filter
     }
 }
