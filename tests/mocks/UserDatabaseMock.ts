35c959da40c3a2e1d3bacb0e92ee7afebd077a15
Francine Hahn <fran_hahn@hotmail.com>
2023-03-07 16:22:46 -0300

    I included the req and res params in the routers

---
+++ tests/mocks/UserDatabaseMock.ts
@@ -0,0 +1,20 @@
+import { UserRepository } from "../../src/business/UserRepository"
+import User, { updateUserInfoDTO } from "../../src/models/User"
+
+
+export default class UserDatabaseMock implements UserRepository {
+
+    signup = async (newUser: User): Promise<void> => {
+        
+    }
+
+
+    editUserInfo = async (userInfo: updateUserInfoDTO): Promise<void> => {
+        
+    }
+
+
+    getUserBy = async (column: string, value: string): Promise<any> => {
+        
+    }
+}
---
+++ tests/mocks/UserMock.ts
@@ -0,0 +1,3 @@
+import User from "../../src/models/User"
+
+export const UserMock = new User("id", "nome teste", "teste@email.com", "123456")
