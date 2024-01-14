a401130e08a6046593f44ec59f1ac57d7473fcac
Francine Hahn <fran_hahn@hotmail.com>
2024-01-14 17:47:06 -0300

    refactor: cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export const app = express()
 app.use(express.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://ecommerce-frontend-eight.vercel.app')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
     next()
 })
