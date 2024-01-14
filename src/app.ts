4e1736a7e88aa4b3a9f13cab2e898ee3f40a3915
Francine Hahn <fran_hahn@hotmail.com>
2024-01-14 17:38:10 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ dotenv.config()
 
 export const app = express()
 app.use(express.json())
-app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ecommerce-frontend-eight.vercel.app')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
 
 app.listen(process.env.PORT || 3003, () => {
     console.log(`Server is running in http://localhost:${process.env.PORT || 3003}`)
